8a95904d8ed7029f5cd2f5ae14ad1d37dd299bf5
gsrunion <gsrunion@gmail.com>
2023-05-16 11:06:58 -0400

    Initial commit

---
+++ src/test/java/com/runion/githubactionstemplate/GithubActionsTemplateApplicationTests.java
@@ -0,0 +1,14 @@
+package com.runion.githubactionstemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class GithubActionsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
